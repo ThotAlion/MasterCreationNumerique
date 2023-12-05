6440db424bfc5e3da3f8a1e84af8f0c08a59d27f
tpeyruse <thomas.peyruse@gmail.com>
2023-12-05 23:18:18 +0100

    nouvelle version du pupitre

--- pupitre/index.js
+++ pupitre/index.js
@@ -1,9 +1,6 @@
-console.log("top");
-var listMotors = ["m1", "m2", "m3", "m4", "m5", "m6"];
 // IP of the robot
-var IProbot = "10.0.0.110:8080";
+var IProbot = "192.168.1.151:5000";
 var mesPos = 0;
-var active = false;
 
 // function to make a pause. Delay is in milliseconds
 function sleep(delay) {
@@ -12,156 +9,79 @@ function sleep(delay) {
 }
 
 // function to led all the leds of the robot to a certain color (off sets the LED off)
-setLed = function (IP, listLED) {
-    for (im = 0; im < listMotors.length; im++) {
-        m = listMotors[im];
-        val = listLED[im];
-        fetch("http://" + IP + "/motors/" + m + "/registers/led/value.json", {
-            method: 'POST',
-            body: '"' + val + '"',
-            headers: { "Content-Type": "application/json;charset=UTF-8" }
-        })
-    }
+setLed = function(IP,listLED){
+    valstr = JSON.stringify(listLED);
+    fetch("http://"+IProbot+"/set_leds",{
+        method: 'POST',
+        body: valstr,
+        headers: {"Content-Type":"application/json;charset=UTF-8"}
+    })
+    .catch(err => console.log(err));
 }
 
-// function to enable the motors to the current position and set moving speed to 0 (which means the higher speed possible)
-enable = function (IP) {
-    active = true;
-    for (im = 0; im < listMotors.length; im++) {
-        m = listMotors[im];
-        fetch("http://" + IP + "/motors/" + m + "/registers/compliant/value.json", {
-            method: 'POST',
-            body: "false",
-            headers: { "Content-Type": "application/json;charset=UTF-8" }
-        })
-            .catch(err => console.log(err))
-        fetch("http://" + IP + "/motors/" + m + "/registers/moving_speed/value.json", {
-            method: 'POST',
-            body: "0",
-            headers: { "Content-Type": "application/json;charset=UTF-8" }
-        })
-            .catch(err => console.log(err))
-    }
-}
-
-// function to disble the motors 
-disable = function (IP) {
-    active = false;
-    for (im = 0; im < listMotors.length; im++) {
-        m = listMotors[im];
-        fetch("http://" + IP + "/motors/" + m + "/registers/compliant/value.json", {
-            method: 'POST',
-            body: "true",
-            headers: { "Content-Type": "application/json;charset=UTF-8" }
-        })
-            .catch(err => console.log(err))
-    }
+// function to disble the motors
+disable = function(IP){
+    setPos(IProbot,[[NaN,NaN,NaN,NaN,NaN,NaN,1]]);
 }
 
 // function to set the motors to a given position in degrees
-setPos = function (IP, listPos) {
-    for (im = 0; im < listMotors.length; im++) {
-        m = listMotors[im];
-        val = listPos[im];
-        if (~isNaN(val)) {
-            fetch("http://" + IP + "/motors/" + m + "/registers/goal_position/value.json", {
-                method: 'POST',
-                body: val.toString(),
-                headers: { "Content-Type": "application/json;charset=UTF-8" }
-            })
-                .catch(err => console.log(err))
-        }
-    }
+setPos = function(IP,listPos){
+    valstr = JSON.stringify(listPos);
+    fetch("http://"+IProbot+"/set_positions",{
+        method: 'POST',
+        body: valstr,
+        headers: {"Content-Type":"application/json;charset=UTF-8"}
+    })
+    .catch(err => console.log(err));
 }
 
 // function to recup the motor positions in a variable mesPos
-getPos = function (IP) {
-    mesPos = {};
-    for (im = 0; im < listMotors.length; im++) {
-        let m = listMotors[im];
-        fetch("http://" + IP + "/motors/" + m + "/registers/present_position/value.json", {
-            method: 'GET',
-            headers: { "Content-Type": "application/json;charset=UTF-8" }
-        })
-            .then(res => res.json())
-            .then(data => {
-                mesPos[m] = Math.round(data.present_position);
-                document.getElementById("positions").innerHTML = "[" + mesPos.m1 + "," + mesPos.m2 + "," + mesPos.m3 + "," + mesPos.m4 + "," + mesPos.m5 + "," + mesPos.m6 + "]";
-            });
-    }
+getPos = function(IP){
+    mesPos={};
+    fetch("http://"+IProbot+"/get_positions",{
+        method: 'GET',
+        headers: {"Content-Type":"application/json;charset=UTF-8"}
+    })
+    .then(res => res.json())
+    .then(data => {
+        mesPos.m1 = Math.round(data[0]);
+        mesPos.m2 = Math.round(data[1]);
+        mesPos.m3 = Math.round(data[2]);
+        mesPos.m4 = Math.round(data[3]);
+        mesPos.m5 = Math.round(data[4]);
+        mesPos.m6 = Math.round(data[5]);
+        document.getElementById("positions").innerHTML = "["+mesPos.m1+","+mesPos.m2+","+mesPos.m3+","+mesPos.m4+","+mesPos.m5+","+mesPos.m6+"]";
+    })
+    .catch(err => console.log(err));
 }
-
-// function to set the motors to a given position in degrees in a given time
-gotoPos = function(IP,listPos,duree){
-    if(active == false){
-        alert("Le robot n'est pas activé.");
-    }else{
-        if(mesPos==0){
-            alert("Lire les positions pour la première pose.");
-        }else{
-            let t0 = Date.now();
-            while(Date.now()-t0<(duree*1000)){
-                let b=(Date.now()-t0)/(duree*1000);
-                let a = [b*listPos[0]+(1-b)*mesPos.m1,
-                    b*listPos[1]+(1-b)*mesPos.m2,
-                    b*listPos[2]+(1-b)*mesPos.m3,
-                    b*listPos[3]+(1-b)*mesPos.m4,
-                    b*listPos[4]+(1-b)*mesPos.m5,
-                    b*listPos[5]+(1-b)*mesPos.m6]
-                setPos(IP,a);
-                sleep(50);
-            }
-            mesPos.m1 = listPos[0];
-            mesPos.m2 = listPos[1];
-            mesPos.m3 = listPos[2];
-            mesPos.m4 = listPos[3];
-            mesPos.m5 = listPos[4];
-            mesPos.m6 = listPos[5];
-        }
-    }
-}
-// gotoPos = function (IP, listPos, duree) {
-//     val = {}
-//     val.motors = ["m1","m2","m3","m4","m5","m6"];
-//     val.positions = listPos;
-//     val.duration = duree;
-//     val.wait = "true";
-//     console.log(val);
-//     console.log(val.toString());
-//     fetch("http://" + IP + "/motors/goto.json", {
-//         method: 'POST',
-//         body: JSON.stringify(val),
-//         headers: { "Content-Type": "application/json" }
-//     })
-//     .catch(err => console.log(err))
-// }
 
 // interface specification
-
-document.getElementById("bouton1").addEventListener("click", function (evt) {
+document.getElementById("bouton1").addEventListener("click",function (evt){
     disable(IProbot);
 });
-document.getElementById("bouton2").addEventListener("click", function (evt) {
-    enable(IProbot);
+document.getElementById("bouton3").addEventListener("click",function (evt){
+    setLed(IProbot,["off","off","off","off","off","off"]);
 });
-document.getElementById("bouton3").addEventListener("click", function (evt) {
-    setLed(IProbot, ["off", "off", "off", "off", "off", "off"]);
+document.getElementById("bouton4").addEventListener("click",function (evt){
+    setLed(IProbot,["red","red","red","red","red","red"]);
 });
-document.getElementById("bouton4").addEventListener("click", function (evt) {
-    setLed(IProbot, ["red", "red", "red", "red", "red", "red"]);
+document.getElementById("bouton5").addEventListener("click",function (evt){
+    setPos(IProbot,[[0,0,0,0,0,0,2]]);
 });
-document.getElementById("bouton5").addEventListener("click", function (evt) {
-    gotoPos(IProbot, [0,37,20,0,-12,1], 5); //fleur
-});
-document.getElementById("bouton6").addEventListener("click", function (evt) {
-    gotoPos(IProbot, [5,37,-9,-3,58,-21], 1); //init
-    gotoPos(IProbot, [-12,98,-52,-4,-39,54], 1); //init
-    gotoPos(IProbot, [-12,98,-52,-4,-40,-21], 1); //init
-    gotoPos(IProbot, [-9,-55,-53,-11,-48,88], 0.1); //init
-
-
-
-});
-document.getElementById("bouton7").addEventListener("click", function (evt) {
+document.getElementById("bouton6").addEventListener("click",function (evt){
     getPos(IProbot);
+});
+document.getElementById("bouton7").addEventListener("click",function (evt){
+    let chore = []
+    chore.push([0,0,0,0,0,0,2]);
+    chore.push([0,0,0,0,0,40,1.8]);
+    chore.push([0,0,0,0,0,0,1.6]);
+    chore.push([0,0,0,0,0,40,1.4]);
+    chore.push([0,0,0,0,0,0,1.2]);
+    chore.push([0,0,0,0,0,40,1]);
+    chore.push([0,0,0,0,0,0,0.8]);
+    chore.push([0,0,0,0,0,40,0.6]);
+    chore.push([0,0,0,0,0,0,0.4]);
+    chore.push([0,0,0,0,0,40,0.2]);
+    setPos(IProbot,chore);
 });
---
+++ pupitre/poppy_thread.py
@@ -0,0 +1,173 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import dynamixel_sdk as dxl
+import threading
+import json,time,math
+global listPos,t0,posInit
+
+# Initialize Flask app
+app = Flask(__name__)
+CORS(app)
+
+# Configure Dynamixel serial port
+PORT_NAME = '/dev/ttyS0'
+BAUDRATE = 1000000
+PROTOCOL_VERSION = 2.0
+
+ADDR_TORQUE_ENABLE = 24
+ADDR_LED = 25
+ADDR_GOAL_POSITION = 30
+ADDR_PRESENT_POSITION = 37
+TORQUE_ENABLE = 1
+TORQUE_DISABLE = 0
+listPos = []
+t0=time.time()
+
+
+
+# Initialize PortHandler instance
+port_handler = dxl.PortHandler(PORT_NAME)
+
+# Initialize PacketHandler instance
+packet_handler = dxl.PacketHandler(PROTOCOL_VERSION)
+
+# Open port
+if not port_handler.openPort():
+    raise IOError("Failed to open port")
+
+# Set port baudrate
+if not port_handler.setBaudRate(BAUDRATE):
+    raise IOError("Failed to set baud rate")
+for dxl_id in range(1, 7):
+    dxl_comm_result, dxl_error = packet_handler.write1ByteTxRx(port_handler, dxl_id, 29, 32)
+    dxl_comm_result, dxl_error = packet_handler.write1ByteTxRx(port_handler, dxl_id, 28, 0)
+    dxl_comm_result, dxl_error = packet_handler.write1ByteTxRx(port_handler, dxl_id, 27, 0)
+    dxl_comm_result, dxl_error = packet_handler.write2ByteTxRx(port_handler, dxl_id, 32, 0)
+    dxl_comm_result, dxl_error = packet_handler.write1ByteTxRx(port_handler, dxl_id, ADDR_LED, 3)
+    time.sleep(0.1)
+for dxl_id in range(1, 7):
+    dxl_comm_result, dxl_error = packet_handler.write1ByteTxRx(port_handler, dxl_id, ADDR_LED, 0)
+
+# Function to set position
+def set_position(dxl_id, position):
+    if position is not None:
+        # Write goal position
+        posNum = int(512+position*1023/300)
+        dxl_comm_result, dxl_error = packet_handler.write2ByteTxRx(port_handler, dxl_id, ADDR_GOAL_POSITION, posNum)
+        if dxl_comm_result != dxl.COMM_SUCCESS:
+            return packet_handler.getTxRxResult(dxl_comm_result)
+        elif dxl_error != 0:
+            return packet_handler.getRxPacketError(dxl_error)
+    else:
+        # Disable torque for this motor
+        dxl_comm_result, dxl_error = packet_handler.write1ByteTxRx(port_handler, dxl_id, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+        if dxl_comm_result != dxl.COMM_SUCCESS:
+            return packet_handler.getTxRxResult(dxl_comm_result)
+        elif dxl_error != 0:
+            return packet_handler.getRxPacketError(dxl_error)
+    return "OK"
+
+def set_led(dxl_id, color):
+    if color=="off":
+        a = 0
+    elif color=="red":
+        a = 1
+    elif color=="green":
+        a = 2
+    elif color=="yellow":
+        a = 3
+    elif color=="blue":
+        a = 4
+    elif color=="purple":
+        a = 5
+    elif color=="cyan":
+        a = 6
+    elif color=="white":
+        a = 7
+    else:
+        a = 0
+    dxl_comm_result, dxl_error = packet_handler.write1ByteTxRx(port_handler, dxl_id, ADDR_LED, a)
+    if dxl_comm_result != dxl.COMM_SUCCESS:
+        return packet_handler.getTxRxResult(dxl_comm_result)
+    elif dxl_error != 0:
+        return packet_handler.getRxPacketError(dxl_error)
+
+# Function to get current position
+def get_position(dxl_id):
+    position, dxl_comm_result, dxl_error = packet_handler.read2ByteTxRx(port_handler, dxl_id, ADDR_PRESENT_POSITION)
+    if dxl_comm_result != dxl.COMM_SUCCESS:
+        return None, packet_handler.getTxRxResult(dxl_comm_result)
+    elif dxl_error != 0:
+        return None, packet_handler.getRxPacketError(dxl_error)
+    return int(-150+position*300/1023),0
+
+posInit = []
+for dxl_id in range(1, 7):  # Assuming motor IDs are 1 to 6
+    position, errmsg = get_position(dxl_id)
+    if position is not None:
+        posInit.append(position)
+    else:
+        posInit.append(0)
+print("posIint:",posInit)
+
+def run_script():
+    global listPos,t0,posInit
+    while True:
+        t=time.time()
+        
+        if len(listPos)>0:
+            print(listPos,t-t0)
+            if (t-t0)>listPos[0][6]:
+                listPos.pop(0)
+                t0=t
+                posInit = []
+                for dxl_id in range(1, 7):  # Assuming motor IDs are 1 to 6
+                    position, errmsg = get_position(dxl_id)
+                    
+                    if position is not None:
+                        posInit.append(position)
+                    else:
+                        posInit.append(0)
+                print("posIint:",posInit)
+            else:
+                for dxl_id in range(1, 7):
+                    a = (t-t0)/listPos[0][6]
+                    if listPos[0][dxl_id-1] is not None:
+                        set_position(dxl_id, (1-a)*posInit[dxl_id-1]+(a)*listPos[0][dxl_id-1])
+                    else:
+                        set_position(dxl_id, None)
+        while time.time()<t+0.04:
+            time.sleep(0.001)
+
+thread = threading.Thread(target=run_script)
+thread.start()
+
+@app.route('/set_positions', methods=['POST'])
+def handle_set_positions():
+    global listPos,t0,posInit
+    content = request.get_json()
+    listPos=[content[0]]+content
+    t0=0
+    return jsonify({"result": "OK"}), 200
+
+@app.route('/set_leds', methods=['POST'])
+def handle_set_leds():
+    content = request.get_json()
+    for dxl_id in range(1, 7):
+        result = set_led(dxl_id, content[dxl_id-1])
+    return jsonify({"result": "OK"}), 200
+
+@app.route('/get_positions', methods=['GET'])
+def handle_get_position():
+    positions = []
+    for dxl_id in range(1, 7):  # Assuming motor IDs are 1 to 6
+        position, errmsg = get_position(dxl_id)
+        if position is not None:
+            positions.append(position)
+        else:
+            positions.append(None)
+    return jsonify(positions), 200
+
+# Run the app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
